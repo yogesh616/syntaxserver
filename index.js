5f5c29d126bd6e4bacc94a6bffb07a26918dfded
Yogesh Saini <71480544+yogesh616@users.noreply.github.com>
2024-11-30 20:38:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
-
 const express = require('express');
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const app = express();
-const port = 3000;
 const cors = require('cors');
-const router = require('./Routes/scrap')
+const router = require('./Routes/scrap');
 
+const app = express();
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors({ origin: '*' }));
 
-
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
 });
 
-
 app.use('/', router);
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
-
+    res.json({message: 'server is running'});
 });
 
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// Export the app instead of listening
+module.exports = app;
